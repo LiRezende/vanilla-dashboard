4168080c43dbb955ee70ed4cee01e0130063e1ea
LiRezende <librusamolin@gmail.com>
2022-08-22 17:55:54 -0300

    Dark theme configured.

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,11 +1,22 @@
 const sideMenu = document.querySelector('aside');
 const menuBtn = document.querySelector('#menu-btn');
 const closeBtn = document.querySelector('#close-btn');
+const themeToggler = document.querySelector('.theme-toggler');
 
+// Open Mobile Menu
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
 })
 
+// Close Mobile Menu
 closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none';
 })
+
+// Apply Dark Theme
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
